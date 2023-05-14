35c50bdd35afd8c7f58c973732c1086aa53ab99f
freedyYang <ever4yong@gmail.com>
2023-05-14 04:14:27 +0000

    clean up

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,30 +11,6 @@
 #
 
 
-# cpufreq
-#sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
-#sed -i 's/services/system/g' package/lean/luci-app-cpufreq/luasrc/controller/cpufreq.lu
-
-# 移除不用软件包
-#rm -rf package/lean/luci-app-dockerman
-#rm -rf package/lean/luci-app-wrtbwmon
-#rm -rf package/lean/luci-theme-argon
-
-# 添加额外软件包
-#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-#git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
-
-
-
-# Add 晶晨宝盒
-#svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
-
-# 科学上网插件依赖
-#svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
-# 编译 po2lmo (如果有po2lmo可跳过)
-#pushd package/luci-app-openclash/tools/po2lmo
-#make && sudo make install
-#popd
 #nas-xunlei
 svn co https://github.com/gngpp/nas-xunlei/trunk/openwrt package/xunlei
 #alist
