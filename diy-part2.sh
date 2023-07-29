76014bbb4016fd990edfd938d74e7fc18bd3db12
freedyYang <ever4yong@gmail.com>
2023-07-29 13:59:32 +0000

    add some luci-apps

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,7 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
 
 svn co https://github.com/gngpp/xunlei/tags/v3.5.2-5/openwrt ./package/xunlei
 
